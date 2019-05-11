1a3ea6f378f55a1db535047d763abff494aa5ce7
FernandaDguez <mfda.da13@gmail.com>
2019-05-10 19:04:30 -0700

    falta agregar cruds y cositas para el manejo de sesión en todo

---
+++ VISTA/src/vista/Main.java
@@ -0,0 +1,17 @@
+package vista;
+
+/**
+ *
+ * @author fernandadominguez
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Window w = new Window();
+        w.setVisible(true);
+    }
+    
+}
